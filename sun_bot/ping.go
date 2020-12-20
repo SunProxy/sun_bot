25d5879a1eb5ba555b375e9db2b97a3194c021c4
jviguy <jviguytwo2@gmail.com>
2020-12-20 15:10:28 -0500

    fixed ping cmd
    Signed-off-by: jviguy <jviguytwo2@gmail.com>

--- sun_bot/ping.go
+++ sun_bot/ping.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/Jviguy/SpeedyCmds/command/ctx"
 	"github.com/bwmarrin/discordgo"
+	"time"
 )
 
 type Ping struct {
@@ -19,8 +20,11 @@ func (p Ping) Setname(newname string) {
 }
 
 func (p Ping) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
+	loc, _ := time.LoadLocation("UTC")
 	em := &discordgo.MessageEmbed{Title: "API Ping!"}
 	em.Fields = make([]*discordgo.MessageEmbedField, 0)
+	em.Color = rgb(211, 20, 124).ToInteger()
+	em.Thumbnail = &discordgo.MessageEmbedThumbnail{URL: sunimg}
 	em.Fields = append(em.Fields, &discordgo.MessageEmbedField{Name: "Websocket Latency: ",
 		Value:  fmt.Sprintf("%vms", session.HeartbeatLatency().Milliseconds()),
 		Inline: false,
@@ -30,15 +34,15 @@ func (p Ping) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
 		Inline: false,
 	})
 	msg, _ := session.ChannelMessageSendEmbed(ctx.GetChannel().ID, em)
+	org, _ := msg.Timestamp.Parse()
 	em.Fields[1] = &discordgo.MessageEmbedField{Name: "HTTP Latency: ",
 		Value:  "Fetching......",
 		Inline: false,
 	}
-	_, _ = session.ChannelMessageEditEmbed(ctx.GetChannel().ID, msg.ID, em)
-	org, _ := msg.Timestamp.Parse()
-	edit, _ := msg.Timestamp.Parse()
+	msg, _ = session.ChannelMessageEditEmbed(ctx.GetChannel().ID, msg.ID, em)
+	edit, _ := msg.EditedTimestamp.Parse()
 	em.Fields[1] = &discordgo.MessageEmbedField{Name: "HTTP Latency: ",
-		Value:  fmt.Sprintf("%vms", org.Sub(edit).Milliseconds()),
+		Value:  fmt.Sprintf("%+vms", edit.In(loc).Sub(org.In(loc)).Milliseconds()),
 		Inline: false,
 	}
 	_, _ = session.ChannelMessageEditEmbed(ctx.GetChannel().ID, msg.ID, em)

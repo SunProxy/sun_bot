704876dfafa6bc46f9e02ff693155639903bd3f2
jviguy <jviguytwo2@gmail.com>
2020-12-20 15:20:10 -0500

    fix up build and embeds
    Signed-off-by: jviguy <jviguytwo2@gmail.com>

--- sun_bot/build.go
+++ sun_bot/build.go
@@ -28,129 +28,142 @@ func (b Build) Setname(newname string) {
 func (b Build) Execute(ctx ctx.Ctx, session *discordgo.Session) error {
 	if len(ctx.GetArgs()) > 0 {
 		num, err := strconv.Atoi(ctx.GetArgs()[0])
-		if err == nil {
-			build, err := client.GetBuild("SunProxy", "sun", num)
-			if err != nil {
-				return fmt.Errorf("unknown build: %v", num)
-			}
-			em := &discordgo.MessageEmbed{Title: "Information on Build " + ctx.GetArgs()[0] + "!"}
-			fields := make([]*discordgo.MessageEmbedField, 0)
-			for _, step := range build.Steps {
-				fields = append(fields, &discordgo.MessageEmbedField{Name: step.Name, Value: step.Actions[0].Status, Inline: true})
-			}
-			em.Fields = fields
-			em.Color = rgb(211, 20, 124).ToInteger()
-			_, err = session.ChannelMessageSendEmbed(ctx.GetChannel().ID, em)
-			return nil
-		}
-		switch ctx.GetArgs()[0] {
-		case "recent":
-			builds, err := client.ListRecentBuildsForProject("SunProxy", "sun", "master", "", -1, 0)
-			if err != nil {
-				log.Fatal(err)
-			}
-			em := &discordgo.MessageEmbed{Title: "All Builds On SunProxy/sun!"}
-			fields := make([]*discordgo.MessageEmbedField, 0)
-			for _, build := range builds {
-				fields = append(fields, &discordgo.MessageEmbedField{
-					Name:   strconv.Itoa(build.BuildNum) + ":",
-					Value:  "Status: " + build.Status + "\nLink: " + build.BuildURL,
-					Inline: false})
-			}
-			em.Fields = fields
-			em.Color = rgb(211, 20, 124).ToInteger()
-			_, err = session.ChannelMessageSendEmbed(ctx.GetChannel().ID, em)
-			return err
-		case "status":
-			if len(ctx.GetArgs()) > 1 {
-				num, err := strconv.Atoi(ctx.GetArgs()[1])
+		if err != nil {
+			switch ctx.GetArgs()[0] {
+			case "latest":
+				build, err := client.ListRecentBuildsForProject("SunProxy", "sun", "master", "", 1, 0)
 				if err != nil {
-					return err
+					return fmt.Errorf("unknown build: %v", ctx.GetArgs()[1])
 				}
-				build, err := client.GetBuild("SunProxy", "sun", num)
-				if err != nil {
-					return fmt.Errorf("unknown build: %v", num)
-				}
-				em := &discordgo.MessageEmbed{Title: "Status of Build " + ctx.GetArgs()[1] + "!"}
+				em := &discordgo.MessageEmbed{Title: "Information on Build " + ctx.GetArgs()[0] + "!"}
 				fields := make([]*discordgo.MessageEmbedField, 0)
-				fields = append(fields, &discordgo.MessageEmbedField{Name: "Status: ", Value: build.Status, Inline: false})
-				fields = append(fields, &discordgo.MessageEmbedField{Name: "Build Link: ", Value: build.BuildURL, Inline: false})
-				fields = append(fields, &discordgo.MessageEmbedField{Name: "Artifacts: ", Value: GenerateArtifactUrl(num), Inline: false})
-				fields = append(fields, &discordgo.MessageEmbedField{Name: "Timing: ",
-					Value:  "from " + build.StartTime.String() + " to " + build.StopTime.String(),
-					Inline: false})
-				fields = append(fields, &discordgo.MessageEmbedField{Name: "Build Starter: ", Value: build.Why, Inline: false})
-				for _, step := range build.Steps {
+				for _, step := range build[0].Steps {
 					fields = append(fields, &discordgo.MessageEmbedField{Name: step.Name, Value: step.Actions[0].Status, Inline: true})
 				}
 				em.Fields = fields
 				em.Color = rgb(211, 20, 124).ToInteger()
-				em.Thumbnail = &discordgo.MessageEmbedThumbnail{URL: build.VCSURL + "/blob/master/SunProxy.png?raw=true"}
 				_, err = session.ChannelMessageSendEmbed(ctx.GetChannel().ID, em)
-				return nil
-			}
-		case "artifacts":
-			if len(ctx.GetArgs()) > 1 {
-				num, err := strconv.Atoi(ctx.GetArgs()[1])
+			case "recent":
+				builds, err := client.ListRecentBuildsForProject("SunProxy", "sun", "master", "", -1, 0)
 				if err != nil {
-					switch ctx.GetArgs()[1] {
-					case "latest":
-						build, err := client.ListRecentBuildsForProject("SunProxy", "sun", "master", "", 1, 0)
-						if err != nil {
-							return fmt.Errorf("unknown build: %v", ctx.GetArgs()[1])
-						}
-						em := &discordgo.MessageEmbed{Title: "Artifacts of Build " + ctx.GetArgs()[1] + "!"}
-						em.Description = "**Artifacts:** [Click Here](" + GenerateArtifactUrl(build[0].BuildNum) + ")"
-						em.Color = rgb(211, 20, 124).ToInteger()
-						_, err = session.ChannelMessageSendEmbed(ctx.GetChannel().ID, em)
-						return nil
-					}
-					return fmt.Errorf("unknown build: %v", ctx.GetArgs()[1])
+					log.Fatal(err)
 				}
-				_, err = client.GetBuild("SunProxy", "sun", num)
-				if err != nil {
-					return fmt.Errorf("unknown build: %v", num)
+				em := &discordgo.MessageEmbed{Title: "All Builds On SunProxy/sun!"}
+				fields := make([]*discordgo.MessageEmbedField, 0)
+				for _, build := range builds {
+					fields = append(fields, &discordgo.MessageEmbedField{
+						Name:   strconv.Itoa(build.BuildNum) + ":",
+						Value:  "Status: " + build.Status + "\nLink: " + build.BuildURL,
+						Inline: false})
 				}
-				em := &discordgo.MessageEmbed{Title: "Artifacts of Build " + ctx.GetArgs()[1] + "!"}
-				em.Description = "**Artifacts:** [Click Here](" + GenerateArtifactUrl(num) + ")"
+				em.Fields = fields
 				em.Color = rgb(211, 20, 124).ToInteger()
 				_, err = session.ChannelMessageSendEmbed(ctx.GetChannel().ID, em)
-				return nil
-			}
-		case "rebuild":
-			if len(ctx.GetArgs()) > 1 && ctx.GetAuthor().ID == "431853152518668294" {
-				num, err := strconv.Atoi(ctx.GetArgs()[1])
-				if err != nil {
-					switch ctx.GetArgs()[1] {
-					case "latest":
-						builds, err := client.ListRecentBuildsForProject("SunProxy", "sun", "master", "", 1, 0)
-						if err != nil {
-							return fmt.Errorf("unknown build: %v", ctx.GetArgs()[1])
-						}
-						build, err := client.RetryBuild("SunProxy", "sun", builds[0].BuildNum)
-						if err != nil {
-							return fmt.Errorf("unknown build: %v", ctx.GetArgs()[1])
-						}
-						em := &discordgo.MessageEmbed{Title: fmt.Sprintf("Rebuilding %v!", build.BuildNum)}
-						em.Description = "Starting now!"
-						em.Color = rgb(211, 20, 124).ToInteger()
-						em.Thumbnail = &discordgo.MessageEmbedThumbnail{URL: build.VCSURL + "/blob/master/SunProxy.png?raw=true"}
-						_, err = session.ChannelMessageSendEmbed(ctx.GetChannel().ID, em)
-						return nil
+				return err
+			case "status":
+				if len(ctx.GetArgs()) > 1 {
+					num, err := strconv.Atoi(ctx.GetArgs()[1])
+					if err != nil {
+						return err
 					}
-					return fmt.Errorf("unknown build: %v", ctx.GetArgs()[1])
+					build, err := client.GetBuild("SunProxy", "sun", num)
+					if err != nil {
+						return fmt.Errorf("unknown build: %v", num)
+					}
+					em := &discordgo.MessageEmbed{Title: "Status of Build " + ctx.GetArgs()[1] + "!"}
+					fields := make([]*discordgo.MessageEmbedField, 0)
+					fields = append(fields, &discordgo.MessageEmbedField{Name: "Status: ", Value: build.Status, Inline: false})
+					fields = append(fields, &discordgo.MessageEmbedField{Name: "Build Link: ", Value: build.BuildURL, Inline: false})
+					fields = append(fields, &discordgo.MessageEmbedField{Name: "Artifacts: ", Value: GenerateArtifactUrl(num), Inline: false})
+					fields = append(fields, &discordgo.MessageEmbedField{Name: "Timing: ",
+						Value:  "from " + build.StartTime.String() + " to " + build.StopTime.String(),
+						Inline: false})
+					fields = append(fields, &discordgo.MessageEmbedField{Name: "Build Starter: ", Value: build.Why, Inline: false})
+					for _, step := range build.Steps {
+						fields = append(fields, &discordgo.MessageEmbedField{Name: step.Name, Value: step.Actions[0].Status, Inline: true})
+					}
+					em.Fields = fields
+					em.Color = rgb(211, 20, 124).ToInteger()
+					em.Thumbnail = &discordgo.MessageEmbedThumbnail{URL: build.VCSURL + "/blob/master/SunProxy.png?raw=true"}
+					_, err = session.ChannelMessageSendEmbed(ctx.GetChannel().ID, em)
+					return nil
 				}
-				build, err := client.RetryBuild("SunProxy", "sun", num)
-				if err != nil {
-					return fmt.Errorf("unknown build: %v", ctx.GetArgs()[1])
+			case "artifacts":
+				if len(ctx.GetArgs()) > 1 {
+					num, err := strconv.Atoi(ctx.GetArgs()[1])
+					if err != nil {
+						switch ctx.GetArgs()[1] {
+						case "latest":
+							build, err := client.ListRecentBuildsForProject("SunProxy", "sun", "master", "", 1, 0)
+							if err != nil {
+								return fmt.Errorf("unknown build: %v", ctx.GetArgs()[1])
+							}
+							em := &discordgo.MessageEmbed{Title: "Artifacts of Build " + ctx.GetArgs()[1] + "!"}
+							em.Description = "**Artifacts:** [Click Here](" + GenerateArtifactUrl(build[0].BuildNum) + ")"
+							em.Color = rgb(211, 20, 124).ToInteger()
+							_, err = session.ChannelMessageSendEmbed(ctx.GetChannel().ID, em)
+							return nil
+						}
+						return fmt.Errorf("unknown build: %v", ctx.GetArgs()[1])
+					}
+					_, err = client.GetBuild("SunProxy", "sun", num)
+					if err != nil {
+						return fmt.Errorf("unknown build: %v", num)
+					}
+					em := &discordgo.MessageEmbed{Title: "Artifacts of Build " + ctx.GetArgs()[1] + "!"}
+					em.Description = "**Artifacts:** [Click Here](" + GenerateArtifactUrl(num) + ")"
+					em.Color = rgb(211, 20, 124).ToInteger()
+					_, err = session.ChannelMessageSendEmbed(ctx.GetChannel().ID, em)
+					return nil
 				}
-				em := &discordgo.MessageEmbed{Title: fmt.Sprintf("Rebuilding %v!", build.BuildNum)}
-				em.Description = "Starting now!"
-				em.Color = rgb(211, 20, 124).ToInteger()
-				em.Thumbnail = &discordgo.MessageEmbedThumbnail{URL: build.VCSURL + "/blob/master/SunProxy.png?raw=true"}
-				_, err = session.ChannelMessageSendEmbed(ctx.GetChannel().ID, em)
+			case "rebuild":
+				if len(ctx.GetArgs()) > 1 && ctx.GetAuthor().ID == "431853152518668294" {
+					num, err := strconv.Atoi(ctx.GetArgs()[1])
+					if err != nil {
+						switch ctx.GetArgs()[1] {
+						case "latest":
+							builds, err := client.ListRecentBuildsForProject("SunProxy", "sun", "master", "", 1, 0)
+							if err != nil {
+								return fmt.Errorf("unknown build: %v", ctx.GetArgs()[1])
+							}
+							build, err := client.RetryBuild("SunProxy", "sun", builds[0].BuildNum)
+							if err != nil {
+								return fmt.Errorf("unknown build: %v", ctx.GetArgs()[1])
+							}
+							em := &discordgo.MessageEmbed{Title: fmt.Sprintf("Rebuilding %v!", build.BuildNum)}
+							em.Description = "Starting now!"
+							em.Color = rgb(211, 20, 124).ToInteger()
+							em.Thumbnail = &discordgo.MessageEmbedThumbnail{URL: build.VCSURL + "/blob/master/SunProxy.png?raw=true"}
+							_, err = session.ChannelMessageSendEmbed(ctx.GetChannel().ID, em)
+							return nil
+						}
+						return fmt.Errorf("unknown build: %v", ctx.GetArgs()[1])
+					}
+					build, err := client.RetryBuild("SunProxy", "sun", num)
+					if err != nil {
+						return fmt.Errorf("unknown build: %v", ctx.GetArgs()[1])
+					}
+					em := &discordgo.MessageEmbed{Title: fmt.Sprintf("Rebuilding %v!", build.BuildNum)}
+					em.Description = "Starting now!"
+					em.Color = rgb(211, 20, 124).ToInteger()
+					em.Thumbnail = &discordgo.MessageEmbedThumbnail{URL: build.VCSURL + "/blob/master/SunProxy.png?raw=true"}
+					_, err = session.ChannelMessageSendEmbed(ctx.GetChannel().ID, em)
+				}
 			}
 		}
+		build, err := client.GetBuild("SunProxy", "sun", num)
+		if err != nil {
+			return fmt.Errorf("unknown build: %v", num)
+		}
+		em := &discordgo.MessageEmbed{Title: "Information on Build " + ctx.GetArgs()[0] + "!"}
+		fields := make([]*discordgo.MessageEmbedField, 0)
+		for _, step := range build.Steps {
+			fields = append(fields, &discordgo.MessageEmbedField{Name: step.Name, Value: step.Actions[0].Status, Inline: true})
+		}
+		em.Fields = fields
+		em.Color = rgb(211, 20, 124).ToInteger()
+		_, err = session.ChannelMessageSendEmbed(ctx.GetChannel().ID, em)
+		return nil
 	}
 	return nil
 }
